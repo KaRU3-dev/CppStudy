8604b8d9b1942812aed6749fef3927ab6c8d4cb3
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2026-01-24 18:57:38 +0900

    チャンクデータのサンプルを追加。

--- 08_DxlibStudy/src/includes/World.h
+++ 08_DxlibStudy/src/includes/World.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "DxLib.h"
 #include "Cube.h"
+#include "PerlinNoise.hpp"
 
 #include <list>
 #include <string>
@@ -9,6 +10,26 @@
 #define WORLD_H
 namespace DxLibStudy
 {
+    struct Chunk
+    {
+        std::list<int> blocks; // 8 x 8 = 64 blocks * 256
+    };
+
+    struct Region
+    {
+        std::list<Chunk> chunks; // 32 * 32 = 1024 chunks
+    };
+
+    // Generate chunk with perlin noise -> https://github.com/Reputeless/PerlinNoise
+    // 1. demention select
+    // 2. biome select
+    // 3. set biome area
+    // 4. generate blocks
+    // *. set cabe area
+    // *. set village area
+    // *. generate nature
+    // WORLD = blocks + cabe + trees + village
+
     struct CubeObject
     {
         int id;
